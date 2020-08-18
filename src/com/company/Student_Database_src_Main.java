81f49701008fe32c499c7f8fbf87764f4b634661
Jaffar Abbas <gamaportal8@gmail.com>
2020-08-19 03:30:34 +0500

    19-8-2020       Problem solved

---
+++ src/com/company/Student_Database_src_Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+
+public class Student_Database_src_Main {
+	    public static void main (String [] args) {
+	        // ask how many students we want to add
+	        System.out.println("How many students do you wish to add? ");
+	        // create n number of new students
+	
+
+	        Student testStudent = new Student();
+	        testStudent.showInfo();
+	
+
+	        testStudent.tuition.enroll();
+	
+
+	        testStudent.tuition.payTuition();
+	
+
+	        testStudent.showInfo();
+	    }
+	}
